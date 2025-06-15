579a8008a5cdfa58208bca3dae1f1e18df14a853
Girish kumar G <66353433+Girish245@users.noreply.github.com>
2025-06-15 16:50:59 +0530

    Update settings.py

--- SmartStreet/settings.py
+++ SmartStreet/settings.py
@@ -144,7 +144,7 @@ STATICFILES_DIRS = [
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'girishkumarg190@gmail.com'
-EMAIL_HOST_PASSWORD = 'rlcwvtieuwfdxkyk'
+EMAIL_HOST_PASSWORD = 'rlccecacwvtieuwedadcfvfdxkyk'
 EMAIL_USE_TLS = True
 
 # Default primary key field type
